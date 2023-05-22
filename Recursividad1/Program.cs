9b1195d0fe8a9e28dd1a1163f3387daab04fca27
Mario Arturo sierra juarez <mario_asj@outlook.com>
2023-05-21 22:04:45 -0600

    Recursividad 1

---
+++ Recursividad1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividad1
+{
+    public class Program
+    {
+        void Imprimir(int x)
+        {
+            if (x > 0)
+            {
+                Imprimir(x - 1);
+                Console.WriteLine(x);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program re = new Program();            
+            re.Imprimir(50);  //Cambiamos el numero por el cual deseamos obtener la recursividad            
+            Console.ReadKey(); 
+        }
+    }
+}
